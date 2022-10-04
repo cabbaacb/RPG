75ebed2bbcf06cdaf98ff458a9a4138b85b338aa
cabbaacb <103894445+cabbaacb@users.noreply.github.com>
2022-10-04 20:49:16 +0300

    added input system

--- Assets/Scripts/Units/Players/PlayerInputComponent.cs
+++ Assets/Scripts/Units/Players/PlayerInputComponent.cs
@@ -2,22 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace RPG.Units
 {
-    public class Unit : MonoBehaviour
+    public class PlayerInputComponent : MonoBehaviour
     {
-        private Animator _animator;
-
         // Start is called before the first frame update
         void Start()
         {
-            _animator = GetComponentInChildren<Animator>();
+
         }
 
         // Update is called once per frame
         void Update()
         {
-            _animator.SetFloat("ForwardMove", 0f);
+
         }
     }
 }
--- Assets/Scripts/Units/UnitInputComponent.cs
+++ Assets/Scripts/Units/UnitInputComponent.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 namespace RPG.Units
 {
-    public class Unit : MonoBehaviour
+    public class UnitInputComponent : MonoBehaviour
     {
-        private Animator _animator;
-
         // Start is called before the first frame update
         void Start()
         {
-            _animator = GetComponentInChildren<Animator>();
+
         }
 
         // Update is called once per frame
         void Update()
         {
-            _animator.SetFloat("ForwardMove", 0f);
+
         }
     }
 }
